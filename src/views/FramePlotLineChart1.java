d4565909d414a22bdfda81f7b5c123c644cecdd7
Pantelis Ioannidis <ioannidis.pant.dev@gmail.com>
2021-02-26 22:09:29 +0200

    map and plot fixes

--- src/views/FramePlotLineChart1.java
+++ src/views/FramePlotLineChart1.java
@@ -20,26 +20,19 @@ import org.jfree.chart.axis.CategoryAnchor;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.category.CategoryItemRenderer;
 import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.DefaultXYDataset;
-import org.jfree.data.xy.XYDataset;
 
 /**
  *
  * @author Pantelis Ioannidis
  */
-public class FramePlotLineChart extends javax.swing.JFrame {
+public class FramePlotLineChart1 extends javax.swing.JFrame {
 
     /**
      * Creates new form FramePlotLineChart
@@ -48,59 +41,55 @@ public class FramePlotLineChart extends javax.swing.JFrame {
     static final String dateFormatPattern = "dd/MM/yyyy";
     SimpleDateFormat simpleDateFormat ;
     
-    public FramePlotLineChart() {
+    public FramePlotLineChart1() {
         initComponents();
         simpleDateFormat = new SimpleDateFormat(dateFormatPattern);
     }
     public void CalculatePlot(PlottingData data){
         this.data = data;
-        final XYDataset dataset = createDataset();
+        final CategoryDataset dataset = createDataset();
         final JFreeChart chart = createChart(dataset);
         final ChartPanel chartPanel = new ChartPanel(chart);
         this.setContentPane(chartPanel);
     }
     
     //Περνάμε τα data σε δομές που καταλαβαίνει η βιβλιοθήκη Jfreechart
-    private XYDataset createDataset() {
+    private CategoryDataset createDataset() {
         
-        final TimeSeriesCollection  dataset = new TimeSeriesCollection ();
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
         //Θα διαπεράσουμε τις 3 λίστες και θα τις προσθέσουμε στο dataset του jfreechart
         //Ελέγχουμε αν είναι σωρευτικά ή καθημερινά δεδομένα για να επιλέξουμε το κατάλληλο πεδίο
-        TimeSeries  series=new TimeSeries("Επιβεβαιωμένα");
-        TimeSeries series2=new TimeSeries("Επιβεβαιωμένα Συνολικά");
+        String date="";
+        String series="Επιβεβαιωμένα";
+        String series2="Επιβεβαιωμένα Συνολικά";
         for(Coviddata dtype : data.getConfirmedList()){
+            date = simpleDateFormat.format(dtype.getTrndate());
             if(data.getShowDailyData()) 
-                series.add(new Day(dtype.getTrndate()),dtype.getQty());
+                dataset.addValue(dtype.getQty(), series, date);
             if(data.getShowAccumulativeData())
-                series2.add(new Day(dtype.getTrndate()),dtype.getProodqty());
+                dataset.addValue(dtype.getProodqty(), series2, date);
         }
-        
-        
-        TimeSeries  series3=new TimeSeries("Ανάρρωσαν");
-        TimeSeries  series4=new TimeSeries("Ανάρρωσαν Συνολικά");
+        String series3="Ανάρρωσαν";
+        String series4="Ανάρρωσαν Συνολικά";
         for(Coviddata dtype : data.getRecoveredList()){
+            date = simpleDateFormat.format(dtype.getTrndate());
             if(data.getShowDailyData()) 
-                series3.add(new Day(dtype.getTrndate()),dtype.getQty());
+                dataset.addValue(dtype.getQty(), series3, date);
             if(data.getShowAccumulativeData())
-                series4.add(new Day(dtype.getTrndate()),dtype.getProodqty());
+                dataset.addValue(dtype.getProodqty(), series4, date);
         }
         
-        
-        TimeSeries  series5=new TimeSeries("Θανατοι");
-        TimeSeries  series6=new TimeSeries("Θανατοι Συνολικά");
+        String series5="Θανατοι";
+        String series6="Θανατοι Συνολικά";
         for(Coviddata dtype : data.getDeathsList()){
+            date = simpleDateFormat.format(dtype.getTrndate());
             if(data.getShowDailyData()) 
-                series5.add(new Day(dtype.getTrndate()),dtype.getQty());
+                dataset.addValue(dtype.getQty(), series5, date);
             if(data.getShowAccumulativeData())
-                series6.add(new Day(dtype.getTrndate()),dtype.getProodqty());
+                dataset.addValue(dtype.getProodqty(), series6, date);
         }
-        if(series.getItemCount()>0) dataset.addSeries(series);   
-        if(series2.getItemCount()>0) dataset.addSeries(series2);
-        if(series3.getItemCount()>0) dataset.addSeries(series3);
-        if(series4.getItemCount()>0) dataset.addSeries(series4);
-        if(series5.getItemCount()>0) dataset.addSeries(series5);
-        if(series6.getItemCount()>0) dataset.addSeries(series6);
+
 
         return dataset;
                 
@@ -108,22 +97,29 @@ public class FramePlotLineChart extends javax.swing.JFrame {
     
     // Εδώ κατασκευάζεται το chart περνόντας το dataset με τα δεδομένα
     // και κάνοντας κάποιες ρυθμίσεις στην εμφάνιση
-    private JFreeChart createChart(XYDataset  dataset) {
-        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+    private JFreeChart createChart(final CategoryDataset dataset) {
+        JFreeChart chart = ChartFactory.createLineChart(
             data.getTitle(),      
             "Ημερομηνία",                    
             "Κρούσματα",                   
-            dataset                 
+            dataset,                   
+            PlotOrientation.VERTICAL,  
+            true,                      
+            true,                      
+            false                      
         );
         
         //Τα χρώματα
         chart.setBackgroundPaint(Color.white);
-        final XYPlot plot = (XYPlot) chart.getPlot();
+        final CategoryPlot plot = (CategoryPlot) chart.getPlot();
         plot.setBackgroundPaint(Color.lightGray);
         plot.setRangeGridlinePaint(Color.white);
         
         
-       
+        CategoryItemRenderer renderer =   plot.getRenderer();
+        renderer.setSeriesVisible(0, true);
+
+        
         //Ρυθμίσης για τον άξονα Υ
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
@@ -131,9 +127,12 @@ public class FramePlotLineChart extends javax.swing.JFrame {
         rangeAxis.setAutoRangeIncludesZero(true);
         
         //Ρυθμίσεις για τον άξονα Χ
-        ValueAxis domain = plot.getDomainAxis();
-        domain.setAutoRange(true);
-              
+        CategoryAxis categoryAxis = (CategoryAxis) plot.getDomainAxis();
+        categoryAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);  
+        categoryAxis.setMaximumCategoryLabelLines(1);
+        
+        
+        
         return chart;
     }
     
@@ -156,6 +155,7 @@ public class FramePlotLineChart extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -175,20 +175,21 @@ public class FramePlotLineChart extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FramePlotLineChart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FramePlotLineChart1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FramePlotLineChart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FramePlotLineChart1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FramePlotLineChart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FramePlotLineChart1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FramePlotLineChart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FramePlotLineChart1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FramePlotLineChart().setVisible(true);
+                new FramePlotLineChart1().setVisible(true);
             }
         });
     }
